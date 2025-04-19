c222ccaff619f7009c11d1d3d934bc2f3b3d941e
Sai Sweth Koormachalam <sai.sweth@cloudfirstgroup.com>
2025-04-19 18:05:52 +0530

    1. Made reusable Row and Button Component. 2. Added default props.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
   padding: 20px;
@@ -13,12 +14,31 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Seaside Oasis</Heading>
-        <Heading as="h2">Check in/out</Heading>
-        <Button onClick={() => console.log("Click")}>Check in</Button>
-        <Button onClick={() => console.log("Click")}>Check in</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Seaside Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in/out</Heading>
+              <Button variation="primary" size="medium">
+                Check in
+              </Button>
+              <Button
+                variation="danger"
+                size="small"
+                onClick={() => console.log("Click")}
+              >
+                Check in
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
