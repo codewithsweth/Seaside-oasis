b81570d3fb69bb7fd53e9b8f3e0c716c48486780
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-04-27 21:24:20 +0530

    1. Installed react hook form. 2. Implemented create cabin form with react query and useForm

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
+
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FileInput from "../../ui/FileInput";
 import Button from "../../ui/Button";
+import { createCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const FormRow = styled.div`
   display: grid;
@@ -36,35 +41,61 @@ const Label = styled.label`
 `;
 
 function CreateCabinForm() {
+  const { register, handleSubmit, reset } = useForm();
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  function onSubmit(data) {
+    mutate(data);
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
+        <Input type="text" id="name" {...register("name")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
+        <Input type="number" id="maxCapacity" {...register("maxCapacity")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
+        <Input type="number" id="regularPrice" {...register("regularPrice")} />
       </FormRow>
       <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" />
+        <Label htmlFor="discountPrice">Discount</Label>
+        <Input type="number" id="discountPrice" {...register("discountPrice")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Input type="text" id="description" />
+        <Input type="text" id="description" {...register("description")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput type="file" id="image/*" />
+        <FileInput
+          type="file"
+          id="image"
+          accept="image/*"
+        //   {...register("image")}
+        />
       </FormRow>
       <FormRow>
-        <Button>Cancel</Button>
-        <Button>Edit Cabin</Button>
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button disabled={isCreating}>Edit Cabin</Button>
       </FormRow>
     </Form>
   );
