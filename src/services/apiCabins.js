b67ebfe95c7e93b2eb7b527e169698fb02119415
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-04-27 19:18:20 +0530

    1. Implemented delete cabin. 2. Renamed cabin -> cabins. 3. Disabled prototype errors

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -8,5 +8,16 @@ export async function getCabins() {
     throw new Error("Cabins could not be loaded")
   }
 
-  return cabins
+  return cabins;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+
+  return data
 }
