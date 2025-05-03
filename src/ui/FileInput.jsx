83ccd69b5379720a7c57db657e4e084f0e6e9a5c
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-05-03 13:35:52 +0530

    1. Made setup for image upload to supabase bucket. 2. cabin image upload completed.

--- src/ui/FileInput.jsx
+++ src/ui/FileInput.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const FileInput = styled.input`
+const FileInput = styled.input.attrs({ type: "file" })`
   font-size: 1.4rem;
   border-radius: var(--border-radius-sm);
 
@@ -17,7 +17,7 @@ const FileInput = styled.input`
     transition: color 0.2s, background-color 0.2s;
 
     &:hover {
-        background-color: var(--color-brand-700);
+      background-color: var(--color-brand-700);
     }
   }
 `;
